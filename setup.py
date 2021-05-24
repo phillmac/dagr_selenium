a910f1aeca13ddc4df0c405af9509cae3065b175
phillmac <phillmackintosh@gmail.com>
2021-05-24 03:10:32 +0000

    Update dagr version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         'docopt == 0.6.2',
-        'dagr_revamped @ git+https://github.com/phillmac/dagr_revamped@0.2.71-dev.0',
+        'dagr_revamped @ git+https://github.com/phillmac/dagr_revamped@0.2.72-dev.0',
         'selenium==3.141.0',
         'aiofiles==0.6.0',
         'aiohttp==3.7.4.post0'
