714b6e6c902aa4e1111f3925b8effbdcdc6b4999
phillmac <phillmackintosh@gmail.com>
2021-06-28 08:59:34 +0000

    FIx version info

--- dagr_selenium/version.py
+++ dagr_selenium/version.py
@@ -1,2 +1,2 @@
-version = '0.1.52.dev0'
-version_info = tuple([int(d) for d in version.split("-")[0].split(".")])
+version = '0.1.53.dev0'
+version_info = tuple([int(d) for d in version.split(".")[:3]])
