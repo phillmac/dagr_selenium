d6863f8224ecddcb60fa94a460c2b29a887e1c72
phillmac <phillmackintosh@gmail.com>
2021-07-13 02:10:37 +0000

    Add dynamic mode

--- dagr_selenium/monitor_trash.py
+++ dagr_selenium/monitor_trash.py
@@ -6,13 +6,16 @@ from .functions import config, manager, monitor_trash
 full_crawl = (environ['FULL_CRAWL'] if environ.get('FULL_CRAWL', None) else input('Full crawl?: ')).lower().startswith('y')
 resort = (environ['RESORT'] if environ.get('RESORT') else input('Resort?: ')).lower().startswith('y')
 
-env_level = environ.get('dagr.riptrash.logging.level', None)
+manager_mode = (environ.get('MANAGER_MODE', 'monitor_trash' + '_full' if full_crawl else ''))
+
+
+env_level = environ.get('dagr.monitortrash.logging.level', None)
 level_mapped = config.map_log_level(
     int(env_level)) if not env_level is None else None
 
 
 
-manager.set_mode('rip_trash')
+manager.set_mode(manager_mode)
 manager.init_logging(level_mapped)
 
 with manager.get_dagr():
