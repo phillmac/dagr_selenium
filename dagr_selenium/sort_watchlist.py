6dd5ca012cc047d6661c4f0ed7a1e57735a298f5
phillmac <phillmac@users.noreply.github.com>
2021-07-18 11:13:41 +1000

    Handle errors

--- dagr_selenium/sort_watchlist.py
+++ dagr_selenium/sort_watchlist.py
@@ -1,9 +1,11 @@
-from os import environ
+import logging
+from os import environ, truncate
 
 from .functions import config, manager, sort_watchlist
 
 
-resort = (environ['RESORT'] if environ.get('RESORT') else input('Resort?: ')).lower().startswith('y')
+resort = (environ['RESORT'] if environ.get('RESORT')
+          else input('Resort?: ')).lower().startswith('y')
 
 env_level = environ.get('dagr.sort_watchlist.logging.level', None)
 level_mapped = config.map_log_level(
@@ -14,4 +16,8 @@ manager.set_mode('sort_watchlist')
 manager.init_logging(level_mapped)
 
 with manager.get_dagr():
-    sort_watchlist(resort=resort)
+    try:
+        sort_watchlist(resort=resort)
+    except:
+        logging.getLogger(__name__).error(
+            'Error while sorting watchlist', exc_info=True)
