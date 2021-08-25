8a177a83b70fc62dc9f8a52b03dd644a8893a7bc
phillmac <phillmackintosh@gmail.com>
2021-08-25 01:23:24 +0000

    Use random subdir, port, refactor ctx managers, add url test

--- tests/intergration/test_queueman.py
+++ tests/intergration/test_queueman.py
@@ -1,43 +1,13 @@
 import logging
 import unittest
-from contextlib import contextmanager
 from pathlib import Path
 from time import sleep
 
 import requests
 
-from tests_setup import (build_selenium, create_net, run_chrome, run_queueman,
-                         setUpTestCase, tearDownTestCase)
-
-
-@contextmanager
-def chrome_network_ctx():
-    chrome_net = create_net('chrome')
-    try:
-        yield chrome_net
-    finally:
-        chrome_net.remove()
-
-
-@contextmanager
-def chrome_container_ctx():
-    chrome_container = run_chrome()
-    try:
-        yield chrome_container
-    finally:
-        chrome_container.stop()
-        chrome_container.remove()
-
-
-def wait_ready():
-    try:
-        resp = requests.get('http://0.0.0.0:3005/ping')
-        resp.raise_for_status()
-        if not resp.json == 'pong':
-            raise Exception('Invalid reply')
-    except (requests.exceptions.ConnectionError):
-        logging.info('Queueman not ready')
-        sleep(0.5)
+from tests_setup import (build_selenium, chrome_container_ctx,
+                         chrome_network_ctx, run_queueman, setUpTestCase,
+                         tearDownTestCase, test_containers_ctx, wait_ready)
 
 
 class TestQueueMan(unittest.TestCase):
@@ -47,6 +17,7 @@ class TestQueueMan(unittest.TestCase):
         self.io = None
         self.container = None
         self.results_dir = None
+        self.container_port = None
 
     def containerLogs(self):
         for log_item in self.container.logs(stdout=True, stderr=True, stream=True, follow=False):
@@ -54,24 +25,31 @@ class TestQueueMan(unittest.TestCase):
 
     def setUp(self):
         setUpTestCase(self, run_queueman)
+        logging.info(f"Using dir {self.results_dir}")
+        logging.info(f"Using port {self.container_port}")
         selenium_dir = self.results_dir.joinpath('.selenium')
         if not selenium_dir.exists():
             selenium_dir.mkdir()
-        sleep(3)
-        # wait_ready()
+        try:
+            wait_ready(self.container_port)
+        except requests.exceptions.HTTPError:
+            self.containerLogs()
+            raise
 
     def test_resolve(self):
         resolved1 = None
         resolved2 = None
         resolved3 = None
+        origin = f"http://0.0.0.0:{self.container_port}"
+
         try:
-            resp1 = requests.get('http://0.0.0.0:3005/resolve',
+            resp1 = requests.get(f"{origin}/resolve",
                                  json={"deviant": "test-acc"})
             resp1.raise_for_status()
             resolved1 = resp1.json().get('resolved', None)
 
             resp2 = requests.get(
-                'http://0.0.0.0:3005/resolve/cache/query', json={"deviant": "test-acc"})
+                f"{origin}/resolve/cache/query", json={"deviant": "test-acc"})
             resp2.raise_for_status()
             resp2_json = resp2.json()
             if isinstance(resp2_json, dict):
@@ -81,9 +59,13 @@ class TestQueueMan(unittest.TestCase):
             sleep(10)
             logging.info('Starting queuman')
             self.container.start()
-            sleep(3)
+            try:
+                wait_ready(self.container_port)
+            except requests.exceptions.HTTPError:
+                self.containerLogs()
+                raise
             resp3 = requests.get(
-                'http://0.0.0.0:3005/resolve/cache/query', json={"deviant": "test-acc"})
+                f"{origin}/resolve/cache/query", json={"deviant": "test-acc"})
             resp3.raise_for_status()
             resp3_json = resp3.json()
             if isinstance(resp3_json, dict):
@@ -97,15 +79,15 @@ class TestQueueMan(unittest.TestCase):
         self.assertTrue(resolved2 == 'Test-acc')
         self.assertTrue(resolved3 == 'Test-acc')
 
-
     def test_queue_items(self):
         result = None
+        endpoint = f"http://0.0.0.0:{self.container_port}/items"
         try:
-            resp = requests.post('http://0.0.0.0:3005/items',
-                                    json=[
-                                        {"mode": "gallery", "deviant": "test-acc"},
-                                        {"mode": "favs", "deviant": "test-acc"}
-                                ])
+            resp = requests.post(endpoint,
+                                 json=[
+                                     {"mode": "gallery", "deviant": "test-acc"},
+                                     {"mode": "favs", "deviant": "test-acc"}
+                                 ])
             resp.raise_for_status()
             result = resp.json()
         except:
@@ -115,6 +97,24 @@ class TestQueueMan(unittest.TestCase):
 
         self.assertTrue(result == 'ok')
 
+    def test_queue_urls(self):
+        results = []
+        endpoint = f"http://0.0.0.0:{self.container_port}/url"
+        urls = [
+            'https://www.deviantart.com/test-acc/gallery/all'
+            'https://www.deviantart.com/test-acc/favourites/all'
+        ]
+        try:
+            for u in urls:
+                resp = requests.post(endpoint, data={'url': u})
+                resp.raise_for_status()
+                results.append(resp.json())
+        except:
+            logging.exception('Failed to queue urls')
+            self.containerLogs()
+            raise
+
+        self.assertTrue(all(r == 'ok' for r in results))
 
     def tearDown(self):
         tearDownTestCase(self)
@@ -124,4 +124,5 @@ if __name__ == '__main__':
     build_selenium()
     with chrome_network_ctx():
         with chrome_container_ctx():
-            unittest.main()
+            with test_containers_ctx():
+                unittest.main()
--- tests/intergration/tests_setup.py
+++ tests/intergration/tests_setup.py
@@ -1,10 +1,15 @@
 import logging
+import random
+import string
+from contextlib import contextmanager
 from pathlib import Path
 from shutil import rmtree
+from time import sleep
 
 import docker
-from dagr_revamped.config import DAGRConfig
+import requests
 from dagr_revamped.builtin_plugins.classes.DAGRHTTPIo import DAGRHTTPIo
+from dagr_revamped.config import DAGRConfig
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=5)
 
@@ -14,8 +19,60 @@ config = DAGRConfig(
 client = docker.from_env()
 client.images.pull('selenium/standalone-chrome')
 
+test_containers = []
+
+
+@contextmanager
+def chrome_network_ctx():
+    chrome_net = create_net('chrome')
+    try:
+        yield chrome_net
+    finally:
+        chrome_net.remove()
+
+
+@contextmanager
+def chrome_container_ctx():
+    chrome_container = run_chrome()
+    try:
+        yield chrome_container
+    finally:
+        chrome_container.stop()
+        chrome_container.remove()
+
+
+@contextmanager
+def test_containers_ctx():
+    try:
+        yield
+    finally:
+        for c in test_containers:
+            try:
+                c.stop()
+                c.remove()
+            except:
+                logging.exception('Unable to cleanup container')
+
+
+def wait_ready(port):
+    waiting = True
+    while waiting:
+        try:
+            resp = requests.get(f"http://0.0.0.0:{port}/ping", timeout=3)
+            resp.raise_for_status()
+            if not resp.json() == 'pong':
+                raise Exception('Invalid reply')
+        except (requests.exceptions.ConnectionError):
+            logging.info('Container not ready')
+            sleep(1)
+        else:
+            waiting = False
+    logging.info('Ready')
+
+
 def create_net(name):
     return client.networks.create(name)
+
 
 def run_chrome():
     return client.containers.run(
@@ -41,7 +98,7 @@ def build_selenium():
     )
 
 
-def run_queueman(output_dir=None):
+def run_queueman(port, output_dir=None):
     return client.containers.run(
         image="phillmac/dagr_selenium",
         command=["-u", "-m", "dagr_selenium.queue_manager"],
@@ -49,11 +106,12 @@ def run_queueman(output_dir=None):
         init=True,
         environment={
             "dagr.plugins.selenium.login_policy": "prohibit",
-            "dagr.plugins.selenium.create_driver_policy": "on-demand-only"
+            "dagr.plugins.selenium.create_driver_policy": "on-demand-only",
+            "QUEUEMAN_LISTEN_PORT": port
         },
         network='chrome',
         ports={
-            '3005/tcp': 3005
+            f"{port}/tcp": port
         },
         user='root',
         volumes={
@@ -62,16 +120,18 @@ def run_queueman(output_dir=None):
         working_dir=output_dir
     )
 
-def run_fnserver(output_dir=None):
+
+def run_fnserver(port, output_dir=None):
     return client.containers.run(
         image="phillmac/dagr_selenium",
         command=["-u", "-m", "dagr_selenium.filenames_server"],
         detach=True,
         init=True,
         environment={
+            "FILENAME_SERVER_LISTEN_PORT": port
         },
         ports={
-            '3002/tcp': 3002
+            f"{port}/tcp": port
         },
         user='root',
         volumes={
@@ -82,14 +142,21 @@ def run_fnserver(output_dir=None):
 
 
 def setUpTestCase(testcase, run_container):
-    testcase.results_dir = Path(__file__, '../../test_results').resolve()
+    random_name = ''.join(random.choices(
+        string.ascii_uppercase + string.digits, k=5))
+    testcase.container_port = random.randint(1025, 9999)
 
-    if not testcase.results_dir.exists():
-        testcase.results_dir.mkdir()
+    testcase.results_dir = Path(
+        __file__, '../../test_results', random_name).resolve()
+    testcase.results_dir.mkdir()
 
-    testcase.container = run_container(str(testcase.results_dir))
+    testcase.container = run_container(
+        testcase.container_port, str(testcase.results_dir))
+    test_containers.append(testcase.container)
+
 
 def tearDownTestCase(testcase):
     testcase.container.stop()
     testcase.container.remove()
+    test_containers.remove(testcase.container)
     rmtree(testcase.results_dir)
