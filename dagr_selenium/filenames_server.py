cf83f3455442f8c309d5ecf7138fa842696aca69
phillmac <phillmackintosh@gmail.com>
2021-12-27 07:11:11 +0000

    Fix import

--- dagr_selenium/filenames_server.py
+++ dagr_selenium/filenames_server.py
@@ -1,5 +1,5 @@
 if __name__ == '__main__':
-    from FilesysAPI.server import run_app
+    from .FilesysAPI.server import run_app
     try:
         asyncio.run(run_app())
     except KeyboardInterrupt:
