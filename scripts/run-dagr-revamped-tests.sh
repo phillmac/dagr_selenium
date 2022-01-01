5c282dee7cf167c21aaddf2bf74e1261e133a21a
phillmac <phillmac@users.noreply.github.com>
2022-01-01 12:28:38 +1100

    Run in host network mode

--- scripts/run-dagr-revamped-tests.sh
+++ scripts/run-dagr-revamped-tests.sh
@@ -5,17 +5,20 @@ docker build . -t phillmac/dagr_revamped_tests -f scripts/dagr_revamped.dockerfi
 docker run --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -e "TEST_IO_CLASS=${TEST_IO_CLASS:-default}" \
+    --net host \
     phillmac/dagr_revamped_tests \
         python3 tests/intergration/test_file_io.py
 
 docker run --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -e "TEST_IO_CLASS=${TEST_IO_CLASS:-default}" \
+    --net host \
     phillmac/dagr_revamped_tests \
         python3 tests/intergration/test_lock_io.py
 
 docker run --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -e "TEST_IO_CLASS=${TEST_IO_CLASS:-default}" \
+    --net host \
     phillmac/dagr_revamped_tests \
         python3 tests/intergration/test_dir_io.py
