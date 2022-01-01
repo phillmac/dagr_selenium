87360b9d281d30c96dd4fdf830c8c7d3e8acc2a5
phillmac <phillmac@users.noreply.github.com>
2022-01-01 13:07:42 +1100

    Build images once only and run tests sequentialy using tagged image

--- scripts/run-dagr-revamped-tests.sh
+++ scripts/run-dagr-revamped-tests.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
-docker build . -t phillmac/dagr_revamped_tests -f scripts/dagr_revamped.dockerfile
-
 docker run --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -e "TEST_IO_CLASS=${TEST_IO_CLASS:-default}" \
+    -e "IMAGE_TAG=${IMAGE_TAG:-latest}" \
     --net host \
     phillmac/dagr_revamped_tests \
         python3 tests/intergration/test_file_io.py
@@ -12,6 +11,7 @@ docker run --rm \
 docker run --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -e "TEST_IO_CLASS=${TEST_IO_CLASS:-default}" \
+    -e "IMAGE_TAG=${IMAGE_TAG:-latest}" \
     --net host \
     phillmac/dagr_revamped_tests \
         python3 tests/intergration/test_lock_io.py
@@ -19,6 +19,7 @@ docker run --rm \
 docker run --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -e "TEST_IO_CLASS=${TEST_IO_CLASS:-default}" \
+    -e "IMAGE_TAG=${IMAGE_TAG:-latest}" \
     --net host \
     phillmac/dagr_revamped_tests \
         python3 tests/intergration/test_dir_io.py
