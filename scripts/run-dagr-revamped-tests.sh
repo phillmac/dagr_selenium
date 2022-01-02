ddf61bff954f87cebb85b3145d7e74e05b5cae0a
phillmac <phillmac@users.noreply.github.com>
2022-01-02 20:46:47 +1100

    Fix cleanup results dir

--- scripts/run-dagr-revamped-tests.sh
+++ scripts/run-dagr-revamped-tests.sh
@@ -4,25 +4,34 @@ docker run --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -e "TEST_IO_CLASS=${TEST_IO_CLASS:-default}" \
     -e "IMAGE_TAG=${IMAGE_TAG:-latest}" \
+    -e "DISABLE_DIR_CLEANUP=TRUE" \
     --net host \
     -v "/dagr_revamped/tests/test_results:/dagr_revamped/tests/test_results" \
     phillmac/dagr_revamped_tests \
         python3 tests/intergration/test_file_io.py
 
-docker run --rm \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -e "TEST_IO_CLASS=${TEST_IO_CLASS:-default}" \
-    -e "IMAGE_TAG=${IMAGE_TAG:-latest}" \
-    --net host \
-    -v "/dagr_revamped/tests/test_results:/dagr_revamped/tests/test_results" \
-    phillmac/dagr_revamped_tests \
-        python3 tests/intergration/test_lock_io.py
+rm -rv /dagr_revamped/tests/test_results
 
 docker run --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -e "TEST_IO_CLASS=${TEST_IO_CLASS:-default}" \
     -e "IMAGE_TAG=${IMAGE_TAG:-latest}" \
+    -e "DISABLE_DIR_CLEANUP=TRUE" \
+    --net host \
+    -v "/dagr_revamped/tests/test_results:/dagr_revamped/tests/test_results" \
+    phillmac/dagr_revamped_tests \
+        python3 tests/intergration/test_lock_io.py
+
+rm -rv /dagr_revamped/tests/test_results
+
+docker run --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -e "TEST_IO_CLASS=${TEST_IO_CLASS:-default}" \
+    -e "IMAGE_TAG=${IMAGE_TAG:-latest}" \
+    -e "DISABLE_DIR_CLEANUP=TRUE" \
     --net host \
     -v "/dagr_revamped/tests/test_results:/dagr_revamped/tests/test_results" \
     phillmac/dagr_revamped_tests \
         python3 tests/intergration/test_dir_io.py
+
+rm -rv /dagr_revamped/tests/test_results
